9e77622589f3eb3da7890bc91b4ecad7988281ce
rejaul rasel <rejaulhasanrasel28@gmail.com>
2023-12-06 00:42:28 +0600

    added delete modal

--- src/pages/Products/Product/Product.jsx
+++ src/pages/Products/Product/Product.jsx
@@ -1,25 +1,79 @@
 import './Product.css'
-
 import { AiOutlineDelete } from "react-icons/ai";
-import { MdStarRate } from "react-icons/md";
 import Rating from './Rating/Rating';
+import Swal from 'sweetalert2';
+
+
 
 const Product = ({ product }) => {
-  const { description, image, price, rating } = product;
-  const stars= rating.rate;
+  const { description, image, price, rating, id,title } = product;
+  const stars = rating.rate;
+  const handleDelete = (id) => {
+    
+    
+    
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success text-light ms-4",
+        cancelButton: "btn btn-light"
+      },
+      buttonsStyling: false,
+      showCloseButton:true,
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Delete Product",
+      text: `Are you sure you want to delete “${title}” from your lists?`,
+      showCancelButton: true,
+      confirmButtonText: "Delete",
+      cancelButtonText: "Cancel",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire({
+          title: "Deleted!",
+          text: "Your product has been deleted.",
+          icon: "success"
+        });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Your imaginary file is safe :)",
+          icon: "error"
+        });
+      }
+    });
+
+
+
+
+
+
+
+    console.log(id)
+    fetch(`https://fakestoreapi.com/products/${id}`,{
+            method:"DELETE"
+        })
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+  }
+
+
   return (
     <div className='product'>
       <div className="hero ">
         <div>
           <div className='flex justify-center'>
-            <img src={image} className="w-40 rounded-lg shadow-2xl" />
+            <img src={image} className="w-40 h-52 rounded-lg shadow-2xl" />
           </div>
           <hr className='
           my-2' />
           <div className='px-5 py-3' >
             <div className='flex justify-between'>
               <p className='font-bold'>${price}</p>
-              <button className='delete_button '><AiOutlineDelete /></button>
+              <button onClick={() => handleDelete(id)} className='delete_button p-2'><AiOutlineDelete /></button>
             </div>
             <Rating stars={stars}></Rating>
             <p className="">{description.slice(0, 50)}</p>
